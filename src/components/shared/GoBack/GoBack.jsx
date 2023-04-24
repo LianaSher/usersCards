aa03cabc73989de91186d7a2f198689dcab198c8
LianaSher <sheremetliana@gmail.com>
2023-04-24 20:45:33 +0300

    correction add

--- src/components/shared/GoBack/GoBack.jsx
+++ src/components/shared/GoBack/GoBack.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import { GoBackLink } from '../GoBack/GoBack.styled';
 export const GoBack = ({ to, children }) => {
   return <GoBackLink to={to}>{children}</GoBackLink>;
